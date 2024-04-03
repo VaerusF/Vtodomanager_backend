fa76e0231d23df67a117be5904a5063fde2626fa
sergey <dgatedever@mail.ru>
2024-04-03 23:17:16 +0300

    [Refactor] Add IsVerified to account entity + nuget update

---
+++ MainApp/Vtodo.DataAccess.Postgres.Migrations/Migrations/20240403200825_AddIsVerifiedToAccountModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vtodo.DataAccess.Postgres.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsVerifiedToAccountModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVerified",
+                table: "Accounts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVerified",
+                table: "Accounts");
+        }
+    }
+}
--- MainApp/Vtodo.Tests/Utils/TestDbUtils.cs
+++ MainApp/Vtodo.Tests/Utils/TestDbUtils.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.InMemory;
 using Vtodo.DataAccess.Postgres;
-using Vtodo.Infrastructure.Interfaces.DataAccess;
 
 namespace Vtodo.Tests.Utils
 {
