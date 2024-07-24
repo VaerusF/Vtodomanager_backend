8fd7a5d1db7ac815e1474ebcfcbcf3aedb7f5db3
sergey <sergeytaynov@gmail.com>
2024-07-24 17:14:43 +0300

    [Refactor] Add LogId field

--- MainApp/Vtodo.Entities/Models/Log.cs
+++ MainApp/Vtodo.Entities/Models/Log.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 using Vtodo.Entities.Enums;
 
 namespace Vtodo.Entities.Models;
 
 public class Log
 {
+    [Key]
+    public Guid LogId { get; set; }
     public string ServiceName { get; set; } = null!;
     public CustomLogLevels LogLevel { get; set; }
     public string Message { get; set; } = null!;
