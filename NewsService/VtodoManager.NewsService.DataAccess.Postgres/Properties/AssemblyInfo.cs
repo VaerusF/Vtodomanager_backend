8e78fac323a75705935f52adb853bcd416f8bdbc
VaerusF <sergeytaynov@mail.ru>
2024-09-27 15:23:02 +0300

    [Refactor] fix project name

--- NewsService/VtodoManager.NewsService.DataAccess.Postgres/Properties/AssemblyInfo.cs
+++ NewsService/VtodoManager.NewsService.DataAccess.Postgres/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("VtodoManager.NewsService.Web")]
 [assembly: InternalsVisibleTo("VtodoManager.NewsService.DataAccess.Postgres.Migrations")]
-[assembly: InternalsVisibleTo("VtodoManager.NewsService.Logger.Tests")]
+[assembly: InternalsVisibleTo("VtodoManager.NewsService.Tests")]
 [assembly: InternalsVisibleTo("VtodoManager.NewsService.Infrastructure.Implementation.Tests.Unit")]
 [assembly: InternalsVisibleTo("VtodoManager.NewsService.UseCases.Tests.Unit")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
--- NewsService/VtodoManager.NewsService.Tests/Properties/AssemblyInfo.cs
+++ NewsService/VtodoManager.NewsService.Tests/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("VtodoManager.NewsService.DataAccess.Postgres")]
 [assembly: InternalsVisibleTo("VtodoManager.NewsService.UseCases")]
 [assembly: InternalsVisibleTo("VtodoManager.NewsService.Web")]
-[assembly: InternalsVisibleTo("VtodoManager.NewsService.Logger.Tests")]
+[assembly: InternalsVisibleTo("VtodoManager.NewsService.Tests")]
 [assembly: InternalsVisibleTo("VtodoManager.NewsService.Infrastructure.Implementation.Tests.Unit")]
 [assembly: InternalsVisibleTo("VtodoManager.NewsService.UseCases.Tests.Unit")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
